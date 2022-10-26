c6a440d41d920e9bbe5e0f1c8da105bfe2849c0f
GavChap <gavin.chapman@gmail.com>
2022-10-26 16:02:52 +0100

    MQTT

--- python/growatt.py
+++ python/growatt.py
@@ -1,11 +1,27 @@
 
 import os
 import time
+import json
+import paho.mqtt.client as mqtt
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.exceptions import ModbusIOException
 from pprint import pprint
 from sph import SPH
 from influxdb import InfluxDBClient
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connected OK Returned code=",rc)
+    else:
+        print("Bad connection Returned code=",rc)
+
+mqtt_client = mqtt.Client("inverter")
+mqtt_client.on_connect = on_connect
+mqtt_client.connect("mosquitto")
+mqtt_client.loop_start()
+
+mqtt_client.publish('homeassistant/sensor/growattmqtt/config',payload='{"name": "Growatt MQTT"}')
+mqtt_client.publish('homeassistant/sensor/growattmqtt/pvPower/config',payload='{"device_class" : "power","name" : "PV Power","state_topic" : "inverter/data/pvPower","unit_of_measurement" : "W"}', retain=True)
 
 port = '/dev/ttyUSB0'
 client = ModbusClient(method='rtu', port=port, baudrate=9600, stopbits=1, parity='N', bytesize=8, timeout=1)
@@ -28,5 +44,10 @@ while True:
         "measurement": "inverter",
         "fields": inv_stats | bat_stats
     }], time_precision='s')
-    
+  
+    stats = inv_stats | bat_stats
+
+    for key, value in stats.items():
+        mqtt_client.publish("inverter/data/" + key, payload=value)
+
     time.sleep(1)
